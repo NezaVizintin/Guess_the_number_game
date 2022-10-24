2cc0fb867f336d72700eb0925331e00bb7ca9adc
Neža Vižintin <neza.vizintin@yahoo.com>
2022-10-24 17:43:05 +0200

    Finished main functionality, added comments
    It's now a basic beta game with all the main components.
    
    (The game still shows the number guessed and the secret number because there is one thing I still have to figure out.)

--- functions.py
+++ functions.py
@@ -7,81 +7,83 @@ def number_secret_generate():
     secret = random.randint(1, 30)
     return secret
 
-def run_game_easy():
-    wrong_guesses = []
-    name = input("Enter player name: ")
-    attempts = 0
-    while True:
-        print()
-        print("Previous guesses: " + str(wrong_guesses))
-        guess = input("Guess the secret number (between 1 and 30): ")
-        print()
-
-        try:
-            guess = int(guess)
-        except:
-            print("Oops, that's not a valid number. Please enter a number between (including) 1 and 30.")
-
-        if guess == secret:
-            score_list = get_top_score()
-            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses})
-            with open("score_list.json", "w") as score_file:
-                score_file.write(json.dumps(score_list))
-            print("You guessed it! The secret number is " + str(guess))
-            print("Attempts needed: " + str(attempts))
-            break
-        elif guess < secret:
-            wrong_guesses.append(guess)
-            attempts += 1
-            print("Not quite there, try going a bit higher!")
-        elif guess > secret:
-            wrong_guesses.append(guess)
-            attempts += 1
-            print("Not quite there, try going a bit lower!")
-
-
-def run_game_hard():
-    wrong_guesses = []
-    secret = random.randint(1, 30)
-    name = input("Enter player name: ")
-    attempts = 0
-    while True:
-        print()
-        print("Previous guesses: " + str(wrong_guesses))
-        guess = input("Guess the secret number (between 1 and 30): ")
-        print()
-
-        try:
-            guess = int(guess)
-
-            if guess == secret:
-                score_list = get_top_score()
-                # -------- improve score list so that it stores game difficulty and shows top results for each  --------
-                score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses})
-                with open("score_list.json", "w") as score_file:
-                    score_file.write(json.dumps(score_list))
-                print("You guessed it! The secret number is " + str(guess))
-                print("Attempts needed: " + str(attempts))
-                break
-            elif guess < secret:
-                wrong_guesses.append(guess)
-                attempts += 1
-                print("Not quite there, try again!")
-            elif guess > secret:
-                wrong_guesses.append(guess)
-                attempts += 1
-                print("Not quite there, try again!")
-        except:
-            print("Oops, that's not a valid number. Please enter a number between (including) 1 and 30.")
-
-def get_top_score(): # retrieves score list
-    with open("score_list.json", "r") as score_file:  # opens score file to read
-        return json.loads(score_file.read())
-
-def top_score(): # prints top 3 scores
-    print("Top scores: ")
-    score_list = get_top_score()
-    if score_list: # checks it score_list is empty
-        score_list.sort(key=operator.itemgetter('attempts')) #sorts score_list by best score ascending
-        for x in range(3): # shows top 5 results
-            print(score_list[x].get("name") + ": " + str(score_list[x]["attempts"]) + " attempts, date: " + score_list[x].get("date"))
+# PREEXISTING CODE FOR REFERENCE
+#
+# def run_game_easy():
+#     wrong_guesses = []
+#     name = input("Enter player name: ")
+#     attempts = 0
+#     while True:
+#         print()
+#         print("Previous guesses: " + str(wrong_guesses))
+#         guess = input("Guess the secret number (between 1 and 30): ")
+#         print()
+#
+#         try:
+#             guess = int(guess)
+#         except:
+#             print("Oops, that's not a valid number. Please enter a number between (including) 1 and 30.")
+#
+#         if guess == secret:
+#             score_list = get_top_score()
+#             score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses})
+#             with open("score_list.json", "w") as score_file:
+#                 score_file.write(json.dumps(score_list))
+#             print("You guessed it! The secret number is " + str(guess))
+#             print("Attempts needed: " + str(attempts))
+#             break
+#         elif guess < secret:
+#             wrong_guesses.append(guess)
+#             attempts += 1
+#             print("Not quite there, try going a bit higher!")
+#         elif guess > secret:
+#             wrong_guesses.append(guess)
+#             attempts += 1
+#             print("Not quite there, try going a bit lower!")
+#
+#
+# def run_game_hard():
+#     wrong_guesses = []
+#     secret = random.randint(1, 30)
+#     name = input("Enter player name: ")
+#     attempts = 0
+#     while True:
+#         print()
+#         print("Previous guesses: " + str(wrong_guesses))
+#         guess = input("Guess the secret number (between 1 and 30): ")
+#         print()
+#
+#         try:
+#             guess = int(guess)
+#
+#             if guess == secret:
+#                 score_list = get_top_score()
+#                 # -------- improve score list so that it stores game difficulty and shows top results for each  --------
+#                 score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "wrong_guesses": wrong_guesses})
+#                 with open("score_list.json", "w") as score_file:
+#                     score_file.write(json.dumps(score_list))
+#                 print("You guessed it! The secret number is " + str(guess))
+#                 print("Attempts needed: " + str(attempts))
+#                 break
+#             elif guess < secret:
+#                 wrong_guesses.append(guess)
+#                 attempts += 1
+#                 print("Not quite there, try again!")
+#             elif guess > secret:
+#                 wrong_guesses.append(guess)
+#                 attempts += 1
+#                 print("Not quite there, try again!")
+#         except:
+#             print("Oops, that's not a valid number. Please enter a number between (including) 1 and 30.")
+#
+# def get_top_score(): # retrieves score list
+#     with open("score_list.json", "r") as score_file:  # opens score file to read
+#         return json.loads(score_file.read())
+#
+# def top_score(): # prints top 3 scores
+#     print("Top scores: ")
+#     score_list = get_top_score()
+#     if score_list: # checks it score_list is empty
+#         score_list.sort(key=operator.itemgetter('attempts')) #sorts score_list by best score ascending
+#         for x in range(3): # shows top 5 results
+#             print(score_list[x].get("name") + ": " + str(score_list[x]["attempts"]) + " attempts, date: " + score_list[x].get("date"))
--- main.py
+++ main.py
@@ -5,23 +5,24 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        number_secret = int(request.cookies.get("number_set"))
-        number_secret = number_secret_generate() if not number_secret else number_secret
+    if request.method == "GET": #what you take from the server
+        number_secret = int(request.cookies.get("number_set")) #get the set secret number
+        number_secret = number_secret_generate() if not number_secret else number_secret #checks if there already is a secret number and generates one if there isn't
 
-        response = make_response(render_template("index.html", number_secret=number_secret))
-        response.set_cookie("number_set", str(number_secret))
+        response = make_response(render_template("index.html", number_secret=number_secret)) #creates a response - renders template including the variable with the secret number
+        response.set_cookie("number_set", str(number_secret)) #creates cookie with the secret number
 
         return response
 
-    elif request.method == "POST":
-        number_secret = int(request.cookies.get("number_set"))
-        number_input = int(request.form.get("number-input"))
+    elif request.method == "POST": #what you tell the server
+        number_secret = int(request.cookies.get("number_set")) #gets cookie with secret number
+        number_input = int(request.form.get("number-input")) #gets user input number from website form
 
-        if number_input == number_secret:
-            response = make_response(render_template("index.html", number="correct", number_secret=number_secret))
+        if number_input == number_secret: # if numbers match, makes response, sets a new secret number and creates a cookie with it
+            response = make_response(render_template("correct.html"))
             number_secret_new = number_secret_generate()
             response.set_cookie("number_set", str(number_secret_new))
+
         else:
             response = make_response(render_template("index.html", number=number_input, number_secret=number_secret))
 
@@ -30,7 +31,7 @@ def index():
 if __name__ == "__main__":
     app.run(use_reloader=True)
 
-# game initialisation:
+# PREEXISTING CODE FOR REFERENCE
 # while True:
 #     main_menu = str(input("Would you like to A) play a new game, B) see the best scores, or C) quit? ")).lower()
 #
